77cbbd7c72f29258a01deaa47760bf626d05348c
yrahull595 <114355514+yrahull595@users.noreply.github.com>
2025-02-06 16:48:21 +0530

    Color Change And Todo List

--- src/components/testComponet.vue
+++ src/components/testComponet.vue
@@ -1,62 +1,174 @@
 <template>
-    <div>
-        <h1 id="hello"> {{ hello }} {{ testing }}</h1>
-        <br />
-        <br />
-        {{ date }}
-        <br />
-        <input v-model="hello">
-        <button v-on:click="clear">Click to clear input field</button>
-        <button v-on:click="makeCapital">Click to convert in UpperCase</button>
-        <p> Has published Books {{ publishedBooksMessage }}</p> 
-    </div>
+  <div id="testing">
+    <p>{{ name }} Testing</p>
+    <div v-bind:style="{ color: color_value.color }">{{ name }} Testing</div>
+    <h1 id="hello">{{ hello }} {{ testing }}</h1>
+    <br />
+    <br />
+    {{ date }}
+    <br />
+    <input v-model="hello" />
+    <button v-on:click="clear">Click to clear input field</button>
+    <button v-on:click="makeCapital">Click to convert in UpperCase</button>
+    <p>Has published Books {{ publishedBooksMessage }}</p>
+    <input
+      :checked="isRed"
+      v-model="isRed"
+      type="radio"
+      v-on:click="red"
+      for="color"
+    />Red
+    <input
+      :checked="isyellow"
+      v-model="isyellow"
+      type="radio"
+      v-on:click="yellow"
+      for="color"
+    />Yellow
+    <input
+      :checked="isGreen"
+      v-model="isGreen"
+      type="radio"
+      v-on:click="green"
+      for="color"
+    />Green
+
+    <br />
+    <input type="text" v-model="name" />
+
+    <select v-model="color_value">
+      <!-- <option v-bind:value="{color: 'red'}">red</option>
+            <option v-bind:value="{color: 'yellow'}">yellow</option>
+            <option v-bind:value="{color: 'black'}">black</option>
+            <option v-bind:value="{color: 'orange'}">orange</option> -->
+      <option
+        v-for="(col, index) in colors"
+        v-bind:key="index"
+        v-bind:value="{ color: col }"
+      >
+        {{ col }}
+      </option>
+    </select>
+    <ul id="lsit">
+      <li v-for="(item, index) in items" :key="item">
+        {{ index }}  - {{ item }} 
+      </li>
+    </ul>
+    <ul id="lsit">
+      <li v-for="(item,name, index) in object" :key="item">
+        {{ index }} - {{ name }} - {{ item }} 
+      </li>
+    </ul>
+    <br />
+    <span v-for="items in numbers" :key="items">
+        {{ items }},
+    </span>
+    
+    <button @click="evenNumbers">even Numbers</button>
+    <div v-if="show">
+    <span  v-for="number in evenNumbersList" :key="number">{{ number }},</span>
+</div>
+<button @click="oddNumbers">odd Numbers</button>
+    <div v-if="shows">
+    <span  v-for="number in oddNumbersList" :key="number">{{ number }},</span>
+</div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "testComponent",
-    data() {
-        return {
-            hello: "",
-            testing: "testing",
-            date: false,
-            author: {
-                name: 'John Doe',
-                books: [
-                    'Vue 2 - Let\'s Grid',
-                    'Vue 3 - Let\'s Grid',
-                    'Vue 4 - Let\'s Grid',
-                ]
-            }
-        };
-    },
-    computed: {
-        publishedBooksMessage() {
-            return this.author.books.length > 0 ? 'Yes' : 'No';
-        }
-    },
-    created() {
-        console.log("Created is Running");
-        this.date = new Date();
-    },
-    mounted() {
-        console.log("Mounted is Running");
-        this.date = new Date();
-    },
-    updated() {
-        console.log("Updated is Running");
+  name: "testComponent",
+  data() {
+    return {
+      hello: "",
+      testing: "testing",
+      date: false,
+      isRed: false,
+      isyellow: false,
+      isGreen: false,
+       show: false,
+       shows: false,
+       evenNumbersList: [],
+       oddNumbersList: [],
+      items: ["Java", "jabvaScript", "HTML", "CSS", "ReactJs", "TailwindCSS"],
+      colors: ["red", "yellow", "black", "orange"],
+      numbers: [1,2,3,4,5,6,7,8,9,10,11,12],
+      color_value: {
+        color: "black",
+      },
+      author: {
+        name: "John Doe",
+        books: [
+          "Vue 2 - Let's Grid",
+          "Vue 3 - Let's Grid",
+          "Vue 4 - Let's Grid",
+        ],
+      },
+      object:{
+        name: "Gautam Yadav",
+        age: "22",
+        email: "test@gmail.com"
+      }
+    };
+  },
+  computed: {
+    publishedBooksMessage() {
+      return this.author.books.length > 0 ? "Yes" : "No";
     },
     
-    methods: {
-        clear() {
-            this.hello = "";
-        },
-        makeCapital() {
-            this.hello = this.hello.toUpperCase();
-        }
-    }
+  },
+  
+  created() {
+    console.log("Created is Running");
+    this.date = new Date();
+  },
+  mounted() {
+    console.log("Mounted is Running");
+    this.date = new Date();
+  },
+  updated() {
+    console.log("Updated is Running");
+  },
+
+  methods: {
+    clear() {
+      this.hello = "";
+    },
+    makeCapital() {
+      this.hello = this.hello.toUpperCase();
+    },
+    red: function () {
+      // eslint-disable-next-line
+      console.log(this.isRed);
+      this.isRed = this.redbox === "yes";
+    },
+    yellow: function () {
+      this.isyellow = this.yellowbox === "yes";
+    },
+    green: function () {
+      this.isGreen = this.greenbox === "yes";
+    },
+    evenNumbers() {
+    this.evenNumbersList = this.numbers.filter(number => number%2 === 0);
+    this.show = true;
+    },
+    oddNumbers() {
+    this.oddNumbersList = this.numbers.filter(number => number%2 != 0);
+    this.shows = true;
+}
+
+  },
 };
 </script>
 
-<style scoped>
+<style >
+.red {
+  color: red;
+}
+.green {
+  color: green;
+}
+.yellow {
+  color: yellow;
+}
 </style>
---
+++ src/components/todo-list.vue
@@ -0,0 +1,70 @@
+<template>
+    <div id="todo-list-example">
+        <form v-on:submit.prevent="addNewTodo">
+            <label for="new-todo">Add a Todo</label>
+            <input
+            v-model="newTodoText"
+            id="new-todo"
+            placeholder="Enter new Todo Task"
+            >
+            <button>Add</button>
+        </form>
+        <ul>
+            <li is ="todo-item" 
+                v-for="(todo, index) in todos"
+                v-bind:key="todo.id"
+                v-bind:title="todo.title"
+                v-on:remove="todos.splice(index,1)"
+                >
+
+            </li>
+
+        </ul>
+
+    </div>
+</template>
+
+<script>
+export default{
+    name: "#todo-list",
+    data() {
+        return {
+     newTodoText: "",
+     todos :
+[
+    {
+        id:1,
+        title: 'Take a Shower'
+    },
+    {
+        id: 2,
+        title: "prepare to cook Food"
+    },
+    {
+        id: 3,
+        title: "Coock food"
+    },
+    {
+        id: 4,
+        title: "Eat food"
+    },
+    {
+        id: 5,
+        title: "Sleep"
+    }
+] ,
+nextTodoId :  6, 
+        }
+ },
+ methods:{
+    addNewTodo : function(){
+        this.toodos.push({
+            id:this.nextTodoId++,
+            title: this.newTodoText
+        })
+        this.newTodoText = ""
+    }
+ }
+
+}
+</script>
